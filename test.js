cdcd9cbf70d6df4b2703edd778d3c27847449903
quoc95 <quoc95pro@gmail.com>
2019-03-07 09:39:09 +0700

    07-03-2019

--- test.js
+++ test.js
@@ -1,15 +1,29 @@
-var Nightmare = require('nightmare');
+const puppeteer = require('puppeteer');
+async function main() {
+const browser = await puppeteer.launch({headless: false});
+const page = await browser.newPage();
+// await page.setViewport({width: 1200, height: 720})
+await page.goto('https://123link.co/auth/signin', { waitUntil: 'networkidle0' }); // wait until page load
+await page.type('#username', 'quoc1995pro@gmail.com');
+await page.type('#password', 'aassdd');
 
-var google = new Nightmare()
-  .goto('http://google.com')
-  .evaluate(function() {
-    return document.querySelector('#SIvCob a')
-      .href;
-  })
-  //run the queue of commands specified, followed by logging the HREF
-  .then(function(result) {
-    console.log(result);
-  })
-  .catch(function(error){
-    console.error('an error has occurred: ' + error);
-  });
+// click and wait for navigation
+await Promise.all([
+          page.click('.btn-flat'),
+          page.waitForNavigation({ waitUntil: 'networkidle0' }),
+]);
+
+// const h3 = await page.evaluate(() => document.querySelector('.bg-aqua .inner p').textContent);
+// console.log(h3);
+
+const elements = await page.$$(".inner h3"); 
+for (let index = 0; index < elements.length; index++) {
+  const data3 = await (await elements[index].getProperty('innerHTML')).jsonValue();
+console.log(data3);
+  
+}
+
+await browser.close();
+}
+
+main();
